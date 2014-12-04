52c51a9a24c024a491954fec7fc349f7bd841368
akeimsuth <akeimsuth@gmail.com>
2014-12-03 22:12:32 -0500

    fixed bitcoin high

--- js/style.js
+++ js/style.js
@@ -1,15 +1,15 @@
 var num;
 var num1;
 var interval = 500;
+//retrieves bitcoin high of the day
 var rate = function(){ 
 $.ajax({
 	dataType: 'json',
-	url: 'http://api.coindesk.com/v1/bpi/currentprice/CNY.json',
-	type: 'GET',
+	url: 'bit.php',
 	cache: false,
 	success: function(data){
-		num=parseFloat(data['bpi']['USD']['rate']).toFixed(2);
-		//$("#rate").html(num.toString());
+		num=parseFloat(data.high).toFixed(2);
+		$("#bit").html(num.toString());
 		setTimeout(function(){
 			rate();
 		},interval);
@@ -19,12 +19,12 @@ $.ajax({
     rate();
 var convert = function(){ 
 $.ajax({
-	dataType: 'json',
-	url: 'https://www.quandl.com/api/v1/datasets/BUNDESBANK/BBEX3_M_JMD_USD_CA_AB_A01.json',
+	dataType: 'jsonp',
+	url: 'http://devel.farebookings.com/api/curconversor/USD/JMD/1/',
 	type: 'GET',
 	cache: false,
 	success: function(data){
-		num1=parseFloat(data['data'][0][1]).toFixed(2);
+		num1=parseFloat(data.JMD).toFixed(2);
 		$("#rate").html(num1.toString());
 		setTimeout(function(){
 			rate();
@@ -36,6 +36,7 @@ $.ajax({
     convert();
 
 function ch(){
+	var mark1=0.01;
 	var x = document.getElementById("test1");
 	var y = document.getElementById("test2");
 	if(x.value =="" || isNaN(x.value)){
@@ -55,16 +56,23 @@ function ch(){
 		var a = (parseFloat(x.value)*(1/num1)).toFixed(2).toString();
 		y.value=a;
 	}else if(document.getElementById("change").value=="jmd" && document.getElementById("change2").value=="btc"){
-		var a = (parseFloat(x.value)*(1/(num*num1))).toFixed(2).toString();
+		var a = (parseFloat(x.value)*(1/(num*num1))).toFixed(5).toString();
 		y.value=a;
 	}else if(document.getElementById("change").value=="btc" && document.getElementById("change2").value=="jmd"){
 		var a = (parseFloat(x.value)*(num*num1)).toFixed(2).toString();
+		y.value=a;
+	}else if(document.getElementById("change").value=="btc" && document.getElementById("change2").value=="usd"){
+		var a = (parseFloat(x.value)*(num)).toFixed(2).toString();
+		y.value=a;
+	}else if(document.getElementById("change").value=="usd" && document.getElementById("change2").value=="btc"){
+		var a = (parseFloat(x.value)*(1/num)).toFixed(5).toString();
 		y.value=a;
 	}
 	
 	}
 }
 function ch2(){
+	var mark2=0.01;
 	var x = document.getElementById("test1");
 	var y = document.getElementById("test2");
 	if(y.value =="" || isNaN(y.value)){
@@ -87,7 +95,13 @@ function ch2(){
 		var a = (parseFloat(y.value)*(num*num1)).toFixed(2).toString();
 		x.value=a;
 	}else if(document.getElementById("change").value=="btc" && document.getElementById("change2").value=="jmd"){
-		var a = (parseFloat(y.value)*(1/(num*num1))).toFixed(2).toString();
+		var a = (parseFloat(y.value)*(1/(num*num1))).toFixed(5).toString();
+		x.value=a;
+	}else if(document.getElementById("change").value=="usd" && document.getElementById("change2").value=="btc"){
+		var a = (parseFloat(y.value)*num).toFixed(2).toString();
+		x.value=a;
+	}else if(document.getElementById("change").value=="btc" && document.getElementById("change2").value=="usd"){
+		var a = (parseFloat(y.value)*(1/num)).toFixed(2).toString();
 		x.value=a;
 	}
 }
